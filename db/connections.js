852bae0673351519094c50438b6d3478d2012e25
Amanda Nelson <amandaln4311@gmail.com>
2021-01-26 22:58:44 -0500

    added modules due to not running code

---
+++ db/connections.js
@@ -0,0 +1,13 @@
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'rootroot',
+    database : 'employees'
+  });
+
+  connection.connect()
+
+  connection.query = util.promisify(connection.query, console.log("connected"))
