c9941cf21331ff232b261f1532b2ea8fcbc48a5d
Amanda Nelson <amandaln4311@gmail.com>
2021-01-27 19:13:34 -0500

    fixed up choices array

--- db/app.js
+++ db/app.js
@@ -6,6 +6,54 @@ require("console.table");
 function init() {
     const logoText = logo ({ name: "Employee Management System"}).render();
     console.log(logoText);
+    start();
 
-}
+function start() {
+    inquirer
+      .prompt({
+        name: "choice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            {
+                name: "View All Employees",
+                value: "VIEW_EMPLOYEES"
+            },
+            {
+                name: "View All Roles",
+                value: "VIEW_ROLE"
+            },
+            {
+                name: "View all Departments",
+                value: "VIEW_DEPARTMENTS"
+            },
+            {
+                name: "Add Department",
+                value: "ADD_DEPARTMENTS"
+            },
+            {
+                name: "Add Employee",
+                value: "ADD_EMPLOYEE"
+            },
+            {
+                name: "Add Role",
+                value: "ADD_ROLE"
+            },
+            {
+                name: "Update Employee",
+                value: "UPDATE_EMPLOYEE"
+            },
+        ]
+      })
+      .then(function(answer) {
+        console.log(answer);
+      })
+  }
+
+  
+
+};
+
+
+
 init()
